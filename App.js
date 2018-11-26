8cb6e5fd4d094584db15a2ff966e705c71bb6297
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-11-25 20:57:07 -0500

    feat: Add Submit button in AddEntry

--- App.js
+++ App.js
@@ -1,62 +1,16 @@
 import React from 'react';
 import {
     View,
-    Text,
     StyleSheet,
-    TouchableHighlight,
-    TouchableNativeFeedback,
-    TouchableOpacity,
-    TouchableWithoutFeedback
 } from 'react-native';
+import AddEntry from "./components/AddEntry";
 
 export default class App extends React.Component {
-
-    handlePress = () => {
-        alert('Hello!')
-    };
 
     render() {
         return (
             <View style={styles.container}>
-                <TouchableHighlight
-                    style={styles.btn}
-                    onPress={this.handlePress}
-                    underlayColor={'#d4271b'}
-                >
-                    <Text style={styles.btnText}>
-                        Touchable Highlight
-                    </Text>
-                </TouchableHighlight>
-
-                <TouchableOpacity
-                    style={styles.btn}
-                    onPress={this.handlePress}
-                >
-                    <Text style={styles.btnText}>
-                        Touchable Opacity
-                    </Text>
-                </TouchableOpacity>
-
-                <TouchableWithoutFeedback
-                    onPress={this.handlePress}
-                >
-                    <View style={styles.btn}>
-                        <Text style={styles.btnText}>
-                            Touchable Without Feedback
-                        </Text>
-                    </View>
-                </TouchableWithoutFeedback>
-
-                <TouchableNativeFeedback
-                    background={TouchableNativeFeedback.SelectableBackground()}
-                    onPress={this.handlePress}
-                >
-                    <View style={styles.btn}>
-                        <Text style={styles.btnText}>
-                            Touchable Native Feedback
-                        </Text>
-                    </View>
-                </TouchableNativeFeedback>
+                <AddEntry/>
             </View>
         );
     }
