dabc97ccdee804fe115cc6666b3c7f3a8406467e
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-11-25 21:46:22 -0500

    feat: Create DB functions

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSlider from "./UdaciSlider";
 import UdaciSteppers from "./UdaciSteppers";
 import {DateHeader} from "./DateHeader";
 import {TextButton} from "./TextButton";
+import {removeEntry, submitEntry} from "../utils/api";
 
 
 function SubmitBtn({onPress}) {
@@ -74,7 +75,7 @@ export default class AddEntry extends Component {
 
         // Navigate to home
 
-        // Save to 'DB'
+        submitEntry({key, entry});
 
         // Clear local notification
     };
@@ -86,7 +87,7 @@ export default class AddEntry extends Component {
 
         // Route to Home
 
-        // Update 'DB'
+        removeEntry(key);
     };
 
     render() {
