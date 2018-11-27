f2ce353f4598399effd673db29686f5f2f10e75e
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-11-26 22:25:27 -0500

    feat: Style the metric icons

--- App.js
+++ App.js
@@ -26,21 +26,5 @@ const styles = StyleSheet.create({
         flex: 1,
         marginLeft: 10,
         marginRight: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    btn: {
-        backgroundColor: '#e53224',
-        margin: 10,
-        padding: 10,
-        paddingLeft: 50,
-        paddingRight: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 5,
-    },
-    btnText: {
-        color: '#fff',
-
-    },
+    }
 });
