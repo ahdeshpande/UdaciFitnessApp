981b8b4d69c7afc404d5affa819321a213e0a8e3
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-11-30 00:26:18 -0500

    feat: Use react-navigation

--- App.js
+++ App.js
@@ -2,21 +2,66 @@ import React from 'react';
 import {
     View,
     StyleSheet,
+    Platform
 } from 'react-native';
-import AddEntry from "./components/AddEntry";
 import {Provider} from "react-redux";
 import {createStore} from "redux/es/redux";
 import reducer from './reducers';
 import History from "./components/History";
+import {createAppContainer, createBottomTabNavigator} from 'react-navigation';
+import {FontAwesome, Ionicons} from '@expo/vector-icons';
+import AddEntry from "./components/AddEntry";
+import {purple, white} from "./utils/colors";
 
-export default class App extends React.Component {
+const Tabs = createBottomTabNavigator({
+    History: {
+        screen: History,
+        navigationOptions: {
+            tabBarLabel: 'History',
+            tabBarIcon: (({tintColor}) => <Ionicons name={'ios-bookmark'}
+                                                    size={30}
+                                                    color={tintColor}/>),
+        }
+    },
+    AddEntry: {
+        screen: AddEntry,
+        navigationOptions: {
+            tabBarLabel: 'Add Entry',
+            tabBarIcon: (({tintColor}) => <FontAwesome name={'plus-square'}
+                                                       size={30}
+                                                       color={tintColor}/>),
+        }
+    }
+}, {
+    navigationOptions: {
+        header: null,
+    },
+    tabBarOptions: {
+        activeTintColor: Platform.OS === 'ios' ? purple : white,
+        style: {
+            height: 56,
+            backgroundColor: Platform.OS === 'ios' ? white : purple,
+            shadowRadius: 6,
+            shadowOpacity: 1,
+            shadowColor: 'rgba(0,0,0,0.24)',
+            shadowOffset: {
+                width: 0,
+                height: 3,
+            },
+        }
+    }
+});
+
+const AppContainer = createAppContainer(Tabs);
+
+class App extends React.Component {
 
     render() {
         return (
             <Provider store={createStore(reducer)}>
                 <View style={styles.container}>
                     <View style={{height: 20}}/>
-                    <History/>
+                    <AppContainer/>
                 </View>
             </Provider>
         );
@@ -30,3 +75,5 @@ const styles = StyleSheet.create({
         marginRight: 10,
     }
 });
+
+export default App;
